974a92323fe5310fcb055aca19d2515872e130b1
nabeen <axnabeen@gmail.com>
2023-06-27 13:52:14 -0500

    Add project files.

---
+++ WorkWithAnyDataStorage/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace WorkWithAnyDataStorage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
